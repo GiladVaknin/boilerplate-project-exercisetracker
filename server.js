349bf58083c7d577daeb5222137f495c156d0085
GiladVaknin <77235037+GiladVaknin@users.noreply.github.com>
2021-03-11 14:39:08 +0200

    POST method added

--- server.js
+++ server.js
@@ -1,18 +1,41 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const bodyParser = require("body-parser");
+const User = require("./users");
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+app.use(express.json());
+app.use(urlencodedParser);
+app.use(cors());
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
+app.post("/api/exercise/new-user", (request, response) => {
+  const username = request.body.username;
 
+  const user = new User({
+    username,
+  });
 
+  user
+    .save()
+    .then((savedUser) => savedUser.toJSON())
+    .then((savedAndFormattedUser) => {
+      response.json(savedAndFormattedUser);
+    })
+    .catch((error) => next(error));
+});
 
+// app.get("/api/exercise/users", (request, response) => {});
+
+// app.post("/api/exercise/add", (request, response) => {});
+
+// app.get("/api/exercise/log", (request, response) => {});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});
---
+++ users.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const url = process.env.MONGODB_URI;
+
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
+
+const userSchema = mongoose.Schema({
+  username: { type: String, required: true },
+});
+
+const User = mongoose.model("users", userSchema);
+
+module.exports = User;
